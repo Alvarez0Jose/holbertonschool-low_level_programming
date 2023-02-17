d29c0ca8a702e13befead220825c3806c83ad5b2
Alvarez0Jose <josealejandro220@gmail.com>
2023-02-17 18:02:51 +0000

    test

---
+++ hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int intType;
+	float floatType;
+	double doubleTyper;
+	char charTyper;
+
+	printf("Size of int: %zu bytes\n", sizeof(intType));
+
+	return (0);
+
