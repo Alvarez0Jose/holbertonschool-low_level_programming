f23b6e0440d6948fc8af57be93d99be531ecf907
Alvarez0Jose <josealejandro220@gmail.com>
2023-02-17 18:12:18 +0000

    going size check

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -9,12 +9,14 @@ int main(void)
 {
 	int intType;
 	float floatType;
-	double doubleType;
+	long int longintType;
+	long long int longlongintType;
 	char charType;
 
 	printf("Size of int: %zu bytes\n", sizeof(intType));
 	printf("Size of float: %zu bytes\n", sizeof(floatType));
-	printf("Size of float: %zu bytes\n", sizeof(doubleType));
+	printf("Size of long int: %zu bytes\n", sizeof(longintType));
+	printf("Size of long long int: %zu bytes\n" sizeof(longlongintType));
 	printf("Size of char: %zu bytes\n", sizeof(charType));
 	return (0);
 }
