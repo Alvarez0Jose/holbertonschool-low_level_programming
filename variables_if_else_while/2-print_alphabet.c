1797f502ba7526681662ee8c5911ac7a8f2536fe
Alvarez0Jose <josealejandro220@gmail.com>
2023-02-21 13:24:47 +0000

    all lowercase

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+	/*print all lowercase letters*/
+	putchar (ch);
+	putchar ('\n');
+	getch();
+
+	for (ch = 'a'; ch <= 'z'; ch++)
