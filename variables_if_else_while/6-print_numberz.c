02013916478a73df5f087a4e5c1e26b4cf5ba771
Alvarez0Jose <josealejandro220@gmail.com>
2023-02-21 16:49:06 +0000

    numbers putchar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int A;
+
+	A = 0;
+	while
+		(A < 10)
+		{
+			putchar(A + '0');
+			A++;
+		}
+	putchar('\n');
+	return (0);
+}
