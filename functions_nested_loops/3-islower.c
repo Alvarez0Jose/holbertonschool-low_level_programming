564388da49aad3efd25812486fe0211b1723e9e6
Alvarez0Jose <josealejandro220@gmail.com>
2023-02-22 20:02:13 +0000

    islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _islower - entry point
+ * Return: 1 (lower case) 0 (otherwise)
+ * @c: placeholder
+*/
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+
+		return (1);
+
+	}
+
+	else
+
+	{
+
+		return (0);
+
+	}
+}
