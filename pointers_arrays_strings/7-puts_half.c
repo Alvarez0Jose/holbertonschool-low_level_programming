e6ab524d41a708bfd4645c48d301b64da15dbfd5
Alvarez0Jose <josealejandro220@gmail.com>
2023-02-28 18:49:38 +0000

    half

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half - prints second half of the string
+ * @str: string to be analized
+ */
+
+void puts_half(char *str)
+{
+	int a = 0;
+	int b, c;
+
+	while (str[a] != '\0')
+	{
+		a++;
+	}
+	if (a % 2 == 0)
+	{
+		b = (a / 2);
+	}
+		else
+			b = (a - 1) / 2;
+	for (c = b; c < a; c++)
+	{
+		_putchar(str[c]);
+	}
+				_putchar('\n');
+}
