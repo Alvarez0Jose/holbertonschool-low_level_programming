433d5e0959a9cbb75972545198c279a000ee3126
Alvarez0Jose <josealejandro220@gmail.com>
2023-03-01 19:26:01 +0000

    compare strings

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strcmp - comparing strings
+ *@s1: compared
+ *@s2: compared
+ *Return: integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int a;
+
+	for (a = 0; s1[a] != '\0' && s2[a] != '\0'; a++)
+	{
+		if (s1[a] != s2[a])
+		{
+			break;
+		}
+	}
+	if (s1[a] < s2[a])
+	{
+		return (s2[a]);
+	}
+	else if (s1[a] > s2[a])
+	{
+		return (s1[a]);
+	}
+	else
+	{
+		return (s1[a] = s2[a]);
+	}
+	return (a);
+}
