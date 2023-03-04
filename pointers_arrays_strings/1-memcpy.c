9d04e5aeb2908515577f1276439b93985704b0a2
Alvarez0Jose <josealejandro220@gmail.com>
2023-03-04 13:15:53 +0000

    memory copy

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_memcpy - copying memory
+ * @dest: destination to be copied
+ * @src: string being copied
+ * @n: elements
+ * Return: (dest)
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *pdest = dest;
+
+	char *psrc = src;
+
+	unsigned int A;
+
+		for (A = 0; n > A; A++)
+		{
+			*pdest++ = *psrc++;
+		}
+	return(dest);
+}
