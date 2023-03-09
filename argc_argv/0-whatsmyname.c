33e7b84347ed9a87b0ea9993155f1211f878d4ed
Alvarez0Jose <josealejandro220@gmail.com>
2023-03-09 20:20:05 +0000

    prints its name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * @argv: argument vecto
+ * @argc: argument counter
+ * Return: int
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf(" %s\n", argv[0]);
+	return (0);
+}
