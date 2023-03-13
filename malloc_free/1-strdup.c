e311ba7e6c20b08261e8441024b587a7642046fe
Alvarez0Jose <josealejandro220@gmail.com>
2023-03-13 17:02:54 +0000

    duplicate

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - string duplicate
+ * @str: string to be analyzed
+ * Return: NULL if insuficient memory, success = pointer to string
+ */
+
+char *_strdup(char *str)
+{
+	int  i;
+	int len;
+	char *strd;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	len = strlen(str);
+	strd = malloc(len);
+
+	if (strd == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < len; i++)
+	{
+		strd[i] = str[i];
+	}
+
+	return (strd);
+}
