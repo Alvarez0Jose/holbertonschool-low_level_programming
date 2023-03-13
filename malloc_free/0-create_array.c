6feb248baaef7f151dc569c20c12b9c4c96400b5
Alvarez0Jose <josealejandro220@gmail.com>
2023-03-13 14:50:48 +0000

    create array

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -18,15 +18,13 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-
-	for (i = 0; i < size; i++)
-	{
-		arr[i] = c;
-	}
-
 	if (arr == NULL)
 	{
 		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
 	}
 	return (arr);
 
