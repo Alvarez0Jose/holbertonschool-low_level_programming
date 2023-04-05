278d4a3c772d2eb636e1d76cc77abc7c6317f15b
Alvarez0Jose <josealejandro220@gmail.com>
2023-04-05 19:12:14 +0000

    ammount of elements

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- *
- *
- *
+ * dlistint_len - ammount of elements
+ * @h: pointer to the list
+ * Return: ammount of elements
  */
 
 size_t dlistint_len(const dlistint_t *h)
