cec5f5cf14bd1986221bceda1cbb37c5c0be1672
Alvarez0Jose <josealejandro220@gmail.com>
2023-04-14 16:09:59 +0000

    fixing return without ()

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -16,18 +16,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 	{
-		return 0;
+		return (0);
 	}
 
 	if (fd == -1)
 	{
-		return 0;
+		return (0);
 	}
 
 	if (buffer == NULL)
 	{
 		close(fd);
-		return 0;
+		return (0);
 	}
 
 
@@ -35,17 +35,17 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		close(fd);
 		free(buffer);
-		return 0;
+		return (0);
 	}
 
 	if (bytesWritten == -1 || bytesWritten != bytesRead)
 	{
 		close(fd);
 		free(buffer);
-		return 0;
+		return (0);
 	}
 
 	close(fd);
 	free(buffer);
-	return bytesWritten;
+	return (bytesWritten);
 }
