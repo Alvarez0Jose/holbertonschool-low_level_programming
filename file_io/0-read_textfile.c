974cbfb29ac704cbf94eac3fb73db3539857dfc5
Alvarez0Jose <josealejandro220@gmail.com>
2023-04-14 15:50:12 +0000

    First task and readme file

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd = open(filename, O_RDONLY);
+	char *buffer = malloc(letters * sizeof(char));
+	ssize_t bytesRead = read(fd, buffer, letters);
+	ssize_t bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
+
+	if (filename == NULL)
+	{
+		return 0;
+	}
+
+	if (fd == -1)
+	{
+		return 0;
+	}
+
+	if (buffer == NULL)
+	{
+		close(fd);
+		return 0;
+	}
+
+
+	if (bytesRead == -1)
+	{
+		close(fd);
+		free(buffer);
+		return 0;
+	}
+
+	if (bytesWritten == -1 || bytesWritten != bytesRead)
+	{
+		close(fd);
+		free(buffer);
+		return 0;
+	}
+
+	close(fd);
+	free(buffer);
+	return bytesWritten;
+}
