83ed40339bd2bb26d418b3306d2f833286f63560
Alvarez0Jose <josealejandro220@gmail.com>
2023-04-16 15:52:59 +0000

    fixing betty style

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,9 +1,9 @@
 #include "hash_tables.h"
 
 /**
- *
- *
- *
+ * hash_table_create - creates an empty hash table
+ *@size: the size of the empty created table
+ *Return: the newly created table
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
